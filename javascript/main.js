8c839bec2edc85f8536b13e996ec6d874d075593
dda <dda@4w8-13.pratt.edu>
2017-02-22 23:42:40 -0500

    V 0.0.2

--- javascript/main.js
+++ javascript/main.js
@@ -2,7 +2,7 @@
 /*------------- Blipp -------------*/
 /*---------------------------------*/
 
-var blipp = require('blippar').blipp;
+blipp = require('blippar').blipp;
 
 blipp.getPeel()
      .setOrientation("portrait")
